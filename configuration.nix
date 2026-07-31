b186d9793efce20382d42ffd5b53f92085490fcd
Calyxanide <cadu3195@gmail.com>
2026-07-31 12:36:49 -0300

    main config

--- configuration.nix
+++ configuration.nix
@@ -7,11 +7,11 @@
 	./custom/nano.nix
 	./custom/hardwareUSR.nix
     ];
-#--------------------Systemd--------------------------------------
-  boot.loader.systemd-boot.enable = true;
+#-----------------------------SYSTEMD-----------------------------
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.enable = true;
 #--------------Nome do Sistema----------------------------------------
-  networking.hostName = "calyxanide";
+  networking.hostName = "terminus";
   networking.networkmanager.enable = true;
  #------------------IDIOMA--------------------------
   time.timeZone = "America/Sao_Paulo";
@@ -30,7 +30,6 @@
    };
   programs.sway = {
 	enable = true;
-#	enableRealtime = true;
 };
   services.displayManager.ly.enable = true;
 #--------------------AUDIO----------------------------
@@ -71,24 +70,15 @@
 	(with pkgs; [
 	wget
 	git
-	swayfx
+	sway
 	alacritty
 	pavucontrol
 	i3blocks
 	]) ++
 	(with pkgs-unstable; [
-	mesa
-	libva
-	intel-media-driver
-	vpl-gpu-rt
-	mangohud
-	intel-gpu-tools
 	intel-compute-runtime
-	gamescope
 	legcord
 	openjdk21
-	limo
-#	vintagestory
 	]);
   fonts = {
   enableDefaultPackages = true;
@@ -96,7 +86,6 @@
 	nerd-fonts.sauce-code-pro
 	nerd-fonts.martian-mono
 	nerd-fonts.lilex
-	perl538Packages.TextLayout
 	pango
 ];
   fontconfig = {
@@ -117,53 +106,52 @@
 	"cfg" = ''cd ~/.config/ && ls'';
 	"sgt" = ''swaymsg -t get_tree'';
 	"fonts" = "fc-list";
-	"steam" = ''gamescope --expose-wayland --adaptive-sync -w 1920 -h 1080 -r 75 -b -- steam'';
 	"reboot" = "sudo systemctl reboot";
 	"nxup" = "sudo nix flake update";
-	"gitnixos" = "./scripts/gitnixos";
+	"nxfps" = "./scripts/gitnixos";
 	};
 };
 #--------------------FSTAB-FILESYSTEM---------------------
-   fileSystems."/boot" = {
-	device = "/dev/disk/by-label/refind";
-	fsType = "vfat";
-	options = [
-	"users"
-	"nofail"
-	"defaults"
-   ];
-};
-   fileSystems."/mnt/nvme" = {
-	device = "/dev/disk/by-label/NVLINUX";
-	fsType = "ext4";
-	options = [
-	"nofail"
-  ];
-};                                                                                  
+#   fileSystems."/boot" = {
+#	device = "/dev/disk/by-label/refind";
+#	fsType = "vfat";
+#	options = [
+#	"users"
+#	"nofail"
+#	"defaults"
+#   ];
+#};
+#   fileSystems."/mnt/nvme" = {
+#	device = "/dev/disk/by-label/NVLINUX";
+#	fsType = "ext4";
+#	options = [
+#	"nofail"
+#  ];
+#};                                                                                  
 #------------CONNECTION----------------------
   networking.firewall.enable = true;
   networking.firewall.allowedTCPPorts = [ 25565 ];
   networking.firewall.allowedUDPPorts = [ 25565 ];
-  networking.firewall.interfaces."ztdiytqva5".allowedTCPPorts = [ 25565 ];
-  networking.firewall.interfaces."ztdiytqva5".allowedUDPPorts = [ 25565 ];
-  networking.firewall.trustedInterfaces = [ "ztdiytqva5" ];
+#  networking.firewall.interfaces."ztdiytqva5".allowedTCPPorts = [ 25565 ];
+#  networking.firewall.interfaces."ztdiytqva5".allowedUDPPorts = [ 25565 ];
+#  networking.firewall.trustedInterfaces = [ "ztdiytqva5" ];
   services.logrotate.checkConfig = false;
   xdg.portal.wlr = {
 	enable = true;	
 };
   services.zerotierone = {
 	enable = true;
-	port = 9993;
+#	port = 9993;
 	joinNetworks = [
 	"cf719fd540a7815d"
  ];
 };
-  nix.sshServe.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDIIBcjCgZ5XVeK8CLElVQoGVW3EtOodbm+qLFRIIzyz cadu3195@gmail.com"];
+#  nix.sshServe.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDIIBcjCgZ5XVeK8CLElVQoGVW3EtOodbm+qLFRIIzyz cadu3195@gmail.com"];
 #---------------OUTROS----------------------------
   environment.sessionVariables = {
-	LIBVA_DRIVER_NAME = "iHD";
+#	LIBVA_DRIVER_NAME = "iHD";
 	WLR_DRM_NO_ATOMIC = 1;
-	WLR_NO_HARDWARE_CURSORS = 1;
+#	WLR_NO_HARDWARE_CURSORS = 1;
 #	WLR_RENDERER = "vulkan";
 	WLR_SCENE_DISABLE_DIRECT_SCANOUT = 1;
 	WLR_DRM_NO_DIRECT_SCANOUT=1;
@@ -179,5 +167,5 @@
   cores = 0;
 };
   security.allowSimultaneousMultithreading = lib.mkForce true;
-  system.stateVersion = "25.11"; 
+  system.stateVersion = "26.05"; 
 }
